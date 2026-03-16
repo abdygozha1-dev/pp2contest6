005284f0214bdffc7024cf435638e1a157c93080
abdygozha1-dev <abdygozha1@gmail.com>
2026-03-16 10:26:57 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,23 +1,19 @@
 #1
-n = int(input())
 nums = list(map(int, input().split()))
 squares = map(lambda x: x*x, nums)
 print(sum(squares))
 
 #2
-n = int(input())
 nums = list(map(int, input().split()))
 evens = list(filter(lambda x: x % 2 == 0, nums))
 print(len(evens))
 
 #3
-n = int(input())
 words = input().split()
 for i, w in enumerate(words):
     print(f"{i}:{w}", end=" ")
 
 #4
-n = int(input())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 dot = sum(x * y for x, y in zip(a, b))
@@ -32,7 +28,6 @@ else:
     print("No")
 
 #6
-n = int(input())
 nums = list(map(int, input().split()))
 if all(x >= 0 for x in nums):
     print("Yes")
@@ -40,19 +35,16 @@ else:
     print("No")
 
 #7
-n = int(input())
 words = input().split()
 longest = max(words, key=len)
 print(longest)
 
 #8
-n = int(input())
 nums = list(map(int, input().split()))
 unique_nums = sorted(set(nums))
 print(*unique_nums)
 
 #9
-n = int(input())
 keys = input().split()
 values = input().split()
 d = dict(zip(keys, values))
@@ -63,7 +55,6 @@ else:
     print("Not found")
 
 #10
-n = int(input())
 nums = list(map(int, input().split()))
 count = sum(map(bool, nums))
 print(count)
