78b8007bd194b33b1b5004ff18913af86877bfae
abdygozha1-dev <abdygozha1@gmail.com>
2026-03-16 10:19:29 +0300

    Refactor input handling and print statements

--- 3.py
+++ 3.py
@@ -1,38 +1,31 @@
 #1
 n = int(input())
 nums = list(map(int, input().split()))
-
-# әр санды квадратқа көтереміз
 squares = map(lambda x: x*x, nums)
-
 print(sum(squares))
+
 #2
 n = int(input())
 nums = list(map(int, input().split()))
-
-# тек жұп сандарды қалдырамыз
 evens = list(filter(lambda x: x % 2 == 0, nums))
-
 print(len(evens))
+
 #3
 n = int(input())
 words = input().split()
-
 for i, w in enumerate(words):
     print(f"{i}:{w}", end=" ")
+
 #4
 n = int(input())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
-
 dot = sum(x * y for x, y in zip(a, b))
-
 print(dot)
+
 #5
 s = input()
-
 vowels = "aeiouAEIOU"
-
 if any(ch in vowels for ch in s):
     print("Yes")
 else:
@@ -41,7 +34,6 @@ else:
 #6
 n = int(input())
 nums = list(map(int, input().split()))
-
 if all(x >= 0 for x in nums):
     print("Yes")
 else:
@@ -50,28 +42,21 @@ else:
 #7
 n = int(input())
 words = input().split()
-
 longest = max(words, key=len)
-
 print(longest)
 
 #8
-
 n = int(input())
 nums = list(map(int, input().split()))
-
 unique_nums = sorted(set(nums))
-
 print(*unique_nums)
+
 #9
 n = int(input())
 keys = input().split()
 values = input().split()
-
 d = dict(zip(keys, values))
-
 query = input()
-
 if query in d:
     print(d[query])
 else:
@@ -80,7 +65,5 @@ else:
 #10
 n = int(input())
 nums = list(map(int, input().split()))
-
 count = sum(map(bool, nums))
-
 print(count)
